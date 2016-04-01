3af05008359394a72ed89719222423426e4dba82
Óscar Bermúdez Garrido <oxcar103@gmail.com>
2016-04-01 12:45:34 +0200

    Esquema de las clases restantes

---
+++ SC/src/sc/TabooSearch.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sc;
+
+import weka.core.Instances;
+
+/**
+ *
+ * @author oxcar103
+ */
+public class TabooSearch extends RandomHeuristic{
+
+    public TabooSearch(Instances inst, int col_class, int seed) {
+        super(inst, col_class, seed);
+    }
+
+    @Override
+    void Exec() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
