9ce1dfb2832c22b4af2190b4747fd64623376529
Óscar Bermúdez Garrido <oxcar103@gmail.com>
2016-06-30 14:08:47 +0200

    Algoritmos meméticos
    Estructura básica

--- SC/src/sc/MemeticAlgorithm.java
+++ SC/src/sc/MemeticAlgorithm.java
@@ -23,4 +23,53 @@ public abstract class MemeticAlgorithm extends GenerationalGA{
         best_childs = best;
     }
     
+    @Override
+    public void Train() {
+        for(int i = 0; i < getPopulation(); i++){
+            RandomSolution();
+            
+            parents.add(car);
+            num_c_sel_parents.add(num_c_sel);
+            eval_parents.add(Evaluate());
+            
+            // Looking for Best Solution
+            if(eval_parents.get(i) > eval_parents.get(index_best_solution)){
+                index_best_solution = i;
+            }
+        }
+        
+        // Let the Population Live and Die
+        for(int j = 0; getEval() < getMaxEval(); j++){
+            // Cleaning the auxiliary vectors
+            childs.clear();
+            num_c_sel_childs.clear();
+            eval_childs.clear();
+            selected_parents.clear();
+            
+            Selection();
+            for(int i = 0; i < selected_parents.size(); i+=2){
+                Crossover(i, i+1);
+            }
+            Mutation();
+            Inheritance();
+            
+            if(j+1 % iteration == 0){
+                if(best_childs){
+                    ImproveBest();
+                }
+                else{
+                    Improve();
+                }
+            }
+        }
+        
+        car = parents.get(index_best_solution);
+        num_c_sel = num_c_sel_parents.get(index_best_solution);
+    }
+    
+    void Improve(){
+    }
+    
+    void ImproveBest(){
+    }
 }
